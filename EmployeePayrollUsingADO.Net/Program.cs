00bbe6f228d78e7cf8b5fe6aad557f5ee2eda886
AnilBamne <anilbamne20@gmail.com>
2022-12-25 13:16:06 +0530

    Adding:Welcome Message

---
+++ EmployeePayrollUsingADO.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollUsingADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employrr Payroll Service Using ADO.Net");
+        }
+    }
+}
