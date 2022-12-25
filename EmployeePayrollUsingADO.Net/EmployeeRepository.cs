2e9edcc307af46de488aefc859459015d11fa4ee
AnilBamne <anilbamne20@gmail.com>
2022-12-25 19:22:45 +0530

    Adding:UC1EstablishingDataBaseConnection code

---
+++ EmployeePayrollUsingADO.Net/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingADO.Net
+{
+    public class EmployeeRepository
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PayrollService;Integrated Security=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+        
+        /// <summary>
+        /// Check connection with data base
+        /// </summary>
+        public void CheckConnection()
+        {
+            connection.Open();
+            Console.WriteLine("Connection Established");
+            this.connection.Close();
+        }
+    }
+}
--- EmployeePayrollUsingADO.Net/Program.cs
+++ EmployeePayrollUsingADO.Net/Program.cs
@@ -7,6 +7,9 @@ namespace EmployeePayrollUsingADO.Net
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employrr Payroll Service Using ADO.Net");
+
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.CheckConnection();
         }
     }
 }
